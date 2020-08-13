bb82a9fa68f2237f4e8d1f0a7cfad517d40ee89f
Tommy Xie <45479889+tommyjcxie@users.noreply.github.com>
2020-08-12 20:26:20 -0400

    part 1 courseinfo exercise

---
+++ courseinfo/src/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) =>
+  parts.map((part) => <Part part={part.name} exercises={part.exercises} />);
+
+const sum = (p1, p2) => p1 + p2;
+const Total = ({ parts }) => (
+  <p>Number of exercises {parts.map((part) => part.exercises).reduce(sum)}</p>
+);
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
